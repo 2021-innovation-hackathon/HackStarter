a5cba84c3770880ed29d965dabe5507503c3b260
ABCXYZ <rnrmfjc@gmail.com>
2021-08-26 02:58:08 +0900

    파이어베이스 테스트 완료!!!

--- src/main.js
+++ src/main.js
@@ -2,5 +2,21 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
+import firebase from "firebase"
+
+var firebaseConfig = {
+    apiKey: "AIzaSyAJce4pFmQJXGz-6Sy1LqSkd8RV0Hanmvs",
+    authDomain: "petsrecipes.firebaseapp.com",
+    projectId: "petsrecipes",
+    storageBucket: "petsrecipes.appspot.com",
+    messagingSenderId: "372618014677",
+    appId: "1:372618014677:web:cb396b90a3afe4a906d75c",
+    measurementId: "G-L3TPHZDYBE"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  firebase.analytics();
+
+
 
 createApp(App).use(store).use(router).mount("#app");
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,18 +3,32 @@
     <Main-img></Main-img>
     <dog-recipe></dog-recipe>
     <cat-recipe></cat-recipe>
+
+    <div> FireBase Test</div>
+
+    <div> FireBase Test</div>
   </div>
 </template>
 <script>
 import MainImg from "../components/Home/MainImg.vue";
 import DogRecipe from "../components/Home/DogRecipe.vue"
 import CatRecipe from "../components/Home/CatRecipe.vue"
+import firebase from "firebase"
+
 export default {
   components: {
     MainImg,
     DogRecipe,
     CatRecipe,
   },
+  setup() {
+    const db = firebase.firestore();
+    db.collection('recipes').get().then((res)=>{
+      res.forEach((ress) => {
+        console.log(ress.data())
+      })
+    })
+  }
 };
 </script>
 <style lang="scss" scoped>
